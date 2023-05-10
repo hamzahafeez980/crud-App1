d2173ad5c7c57d1b90c75e2c97816b724b9627ba
hamzahafeez980 <132979606+hamzahafeez980@users.noreply.github.com>
2023-05-10 15:52:16 +0500

    aa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { EmpAddEditComponent } from './emp-add-edit/emp-add-edit.component';
+import { EmployeeService } from './services/employee.service';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'crud-App';
+export class AppComponent implements OnInit {
+  displayedColumns: string[] = [
+    'id',
+    'firstName',
+    'lastName',
+    'email',
+    'dob',
+    'gender',
+    'education',
+    'company',
+    'experience',
+    'package',
+  ];
+  dataSource!: MatTableDataSource<any>;
 
-  constructor(private _dialog: MatDialog){}
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private _dialog: MatDialog, private _empService: EmployeeService){}
+
+  ngOnInit():void{
+    this.getEmployeeList();
+  }
 
   openEditAddForm(){
     this._dialog.open(EmpAddEditComponent);
 
+  }
+
+  getEmployeeList(){
+    this._empService.getEmployeeList().subscribe({
+      next:(res)=>{
+       this.dataSource = new MatTableDataSource(res);
+       this.dataSource.sort = this.sort;
+       this.dataSource.paginator = this.paginator;
+      },
+      error: console.log,
+    });
   }
   
 }
